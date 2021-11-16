6bfcbebe7535b46cf624f2a0fb8d8a65e03a82ad
Abunuo <liangmingqing315@163.com>
2021-11-16 10:52:49 +0800

    feat(npm): publish npm

--- index.js
+++ index.js
@@ -2,30 +2,38 @@
  * @Date: 2021-11-11 16:49:00
  * @Description: a jsBridge for native
  */
-const createNativeBridge = (nativeBridge, BridgeName = 'jsBridge') => {
+const cenvertData = data => JSON.stringify(data)
+const NOOP = () => {}
+
+const createNativeBridge = (bridgeName = 'jsBridge', nativeBridge = window.ReactNativeWebView) => {
   let callbacks = {};
 
-  window[BridgeName] = {
-    invoke: (type, data) => {
+  window[bridgeName] = {
+    invoke: (type = 'sendMessage', data = {}) => {
+
       // 生成回调函数对应的标识
       const callbackId = new Date().getTime();
 
       callbacks[callbackId] = {
-        success: data.success,
-        fail: data.fail,
+        success: data?.success || NOOP,
+        fail: data?.fail || NOOP
       }
 
-      nativeBridge.postMessage({
-        type,
-        data: data || {},
-        callbackId: callbackId
-      })
+      data.bridgeName = bridgeName
+      data.type = type
+      data.callbackId = callbackId
+      
+      if(nativeBridge) {
+        nativeBridge.postMessage(cenvertData(data));
+      } else {
+        console.error(`nativeBridge is not Found`)
+      }
     },
     receiveMessage: (msg) => {
       // code: 是否成功
       // data: 返回的数据
       // callbackId: 回调函数的标识
-      let { code, data = {}, callbackId } = msg;
+      let { code = 0, data = {}, callbackId } = msg;
 
       if (callbackId && callbacks[callbackId]) {
         if(code == 0) {
@@ -34,12 +42,10 @@ const createNativeBridge = (nativeBridge, BridgeName = 'jsBridge') => {
           callbacks[callbackId].fail(data)
         }
       } else {
-        nativeBridge.postMessage({ // 回调 Native
-          type: 'callback-notfount',
-        })
+        window[bridgeName].invoke('callbackNotFound')
       }
     }
   }
 }
 
-module.exports = createNativeBridge;
+export default createNativeBridge;
